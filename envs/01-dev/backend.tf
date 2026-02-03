5710b9d0d8eadf94a9aec11760bf7d4ef0b578df
abhijeet152 <abhijeetshinde152@gmail.com>
2026-02-03 08:57:35 +0530

    updated backend

--- envs/01-dev/backend.tf
+++ envs/01-dev/backend.tf
@@ -4,8 +4,8 @@ terraform {
     storage_account_name  = "mystorageaccount20260123"
     container_name        = "mycontainer"
     key                   = "dev.terraform.tfstate"
-    lock_duration         = "60m" # Default is 50 minutes
-    lock_renewal_interval = "15m" # Default is 5 minutes
+    # lock_duration         = "60m" # Default is 50 minutes
+    # lock_renewal_interval = "15m" # Default is 5 minutes
     # access_key is better passed via environment variable ARM_ACCESS_KEY
   }
 }
